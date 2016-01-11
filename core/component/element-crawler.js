fb7505ceae3c36b54e9886180a2608377694a8cf
Yu <eternitywisher@gmail.com>
2016-01-11 19:19:04 +1100

    Added element crawler

--- core/component/element-crawler.js
+++ core/component/element-crawler.js
@@ -2,7 +2,22 @@
  * Element's attributes retriever
  */
 function ElementCrawler(){
-    this.getAttributes = function(){
+    this.getAttributes = function(element, attributes){
+        var result = {},
+            i;
         
+        for(i = 0; i < attributes.length; i++){
+            result[attributes[i]] = _getAttribute(element, attributes[i]);
+        }
+        
+        return result;
+    };
+    
+    var _getAttribute = function(element, attribute){
+        if('undefined' !== typeof element[attribute]){
+            return element[attribute];
+        } else {
+            return element.getAttribute(attribute);
+        }
     };
 }
