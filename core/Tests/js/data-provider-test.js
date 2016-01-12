4f3f8649a0901eb7f19d8417d658e29f64722e75
Yu <eternitywisher@gmail.com>
2016-01-12 20:33:05 +1100

    Added data provider
    Data provider responsible for delivering data by inspecting element's
    special properties(value, attribute, etc.)

---
+++ core/Tests/js/data-provider-test.js
@@ -0,0 +1,24 @@
+function getDataProvider(){
+    return new DataProvider(
+        {
+            find: function(){
+                var element = document.createElement('input');
+                
+                element.value = 125;
+                
+                return element;
+            }
+        },
+        //crawler is not used for now @date 12.01.2016
+        {
+            
+        }
+    );
+}
+
+QUnit.test('DataProvider test', function(assert){
+    var provider = getDataProvider();
+    
+    assert.equal(provider.getValue(), '125', 'Testing getValue');
+    assert.notEqual(provider.getValue(), '126', 'Testing getValue, should fail');
+});
---
+++ core/component/data-provider/data-provider.js
@@ -0,0 +1,49 @@
+/**
+ * Provider of DOM element's values
+ * 
+ * @todo DataProvider.getValue should make use of different elements parsers
+ * one for each used tag
+ * 
+ * @param {DOMRepository} domRepository - DOM repository
+ * @param {ElementCrawler} elementCrawler - element crawler
+ */
+function DataProvider(domRepository, elementCrawler){
+    /**
+     * @type DOMRepository
+     */
+    var _domRepository = null;
+    
+    /**
+     * @type ElementCrawler
+     */
+    var _elementCrawler = null;
+    
+    /**
+     * Constructor
+     * 
+     * @param {DOMRepository} domRepository
+     * @param {ElementCrawler} elementCrawler
+     */
+    var _construct = function(domRepository, elementCrawler){
+        _domRepository = domRepository;
+        _elementCrawler = elementCrawler;
+    };
+    
+    /**
+     * Get DOM element value
+     * 
+     * @param {String} identifier - element identifier
+     * @returns {String}
+     */
+    this.getValue = function(identifier){
+        var element = _domRepository.find(identifier);
+        
+        if(!element){
+            throw new Error('Element with identifier "' + identifier + '" is not found');
+        }
+        
+        return element.value;
+    };
+    
+    _construct.call(this, domRepository, elementCrawler);
+}
