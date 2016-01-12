6d649d05d3bfb82b490ea71f0d66443728352604
Yu <eternitywisher@gmail.com>
2016-01-13 03:23:01 +1100

    Added result handler tests

---
+++ core/Tests/js/result-handler-calculus-test.js
@@ -0,0 +1,34 @@
+var value;
+
+function getResultHandlerCalculus(){
+    return new Handler({
+        update: function(){
+            value = 1;
+        }
+    });
+}
+
+function getResult(){
+    return {
+        type: '',
+        data: [{
+            field: '',
+            value: 1
+        }]
+    };
+}
+
+QUnit.test('Result handler calculus', function(assert){
+    var handler = getResultHandlerCalculus(),
+        result = getResult();
+    
+    result.type = 'update-value';
+    assert.equal(handler.supports(result), true, 'Test supports method, should return true');
+    
+    result.type = 'update-attribute';
+    assert.equal(handler.supports(result), false, 'Test supports method, should return false');
+    
+    result.type = 'update-value';
+    handler.handle(result);
+    assert.equal(value, 1, 'Handle test');
+});
