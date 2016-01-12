f118fd2e592f9b02b3140aad2ebae5ad9a01a8ce
Yu <eternitywisher@gmail.com>
2016-01-12 20:33:51 +1100

    Modified getAttribute method
    Now getAttribute method is public

--- core/component/element-crawler.js
+++ core/component/element-crawler.js
@@ -2,18 +2,32 @@
  * Element's attributes retriever
  */
 function ElementCrawler(){
+    /**
+     * Get list of attributes
+     * 
+     * @param {Element} element - element to inspect
+     * @param {String[]} attributes - list of attributes to extract value from
+     * @returns {JSON} {<passed attribute name>: <value>, ...}
+     */
     this.getAttributes = function(element, attributes){
         var result = {},
             i;
         
         for(i = 0; i < attributes.length; i++){
-            result[attributes[i]] = _getAttribute(element, attributes[i]);
+            result[attributes[i]] = this.getAttribute(element, attributes[i]);
         }
         
         return result;
     };
     
-    var _getAttribute = function(element, attribute){
+    /**
+     * Get single attribute value of element
+     * 
+     * @param {Element} element - element to inspect
+     * @param {String} attribute - attribute to extract value from
+     * @returns {String}
+     */
+    this.getAttribute = function(element, attribute){
         if('undefined' !== typeof element[attribute]){
             return element[attribute];
         } else {
