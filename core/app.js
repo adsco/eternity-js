7e629f039a509ae3b4887f277474a283164a0b59
Yu <eternitywisher@gmail.com>
2016-01-19 23:21:57 +1100

    V1 app

--- core/app.js
+++ core/app.js
@@ -1,5 +1,7 @@
 Eternity = {};
-Eternity.App = {};
+Eternity.Core = {};
+Eternity.Core.Handlers = {};
+Eternity.Core.Misc = {};
 Eternity.Components = {};
 Eternity.Components.DOM = {};
 Eternity.Components.DOM.Element = {};
@@ -10,60 +12,226 @@ Eternity.Components.Input.Handler = {};
 Eternity.Components.Output = {};
 Eternity.Components.Output.Resolver = {};
 Eternity.Components.Output.Handler = {};
+Eternity.Container = {};
 Eternity.Helper = {};
 
 Eternity.config = {
     core: {
-        domRepository: {
-            name: '',
-            cls: '',
-            arguments: []
+        DOMRepository: {
+            name: 'dom.repository',
+            cls: 'Eternity.Components.DOM.Repository',
+            args: []
         },
-        elementCrawler: {
-            name: '',
-            cls: '',
-            arguments: []
+        ElementCrawler: {
+            name: 'element.crawler',
+            cls: 'Eternity.Components.DOM.Element.Crawler',
+            args: []
         },
-        resolver: {
-            name: '',
-            cls: '',
-            arguments: []
+        ElementBinder: {
+            name: 'element.binder',
+            cls: 'Eternity.Components.DOM.Element.Binder',
+            args: []
         },
-        resultResolver: {
-            name: '',
-            cls: '',
-            arguments: []
+        InputResolver: {
+            name: 'input.resolver',
+            cls: 'Eternity.Components.Input.Resolver.Resolver',
+            args: ['@output.resolver']
         },
-        domObserver: {
-            name: '',
-            cls: '',
-            arguments: []
+        OutputResolver: {
+            name: 'output.resolver',
+            cls: 'Eternity.Components.Output.Resolver.Resolver',
+            args: []
         },
-        dataProvider: {
-            name: '',
-            cls: '',
-            arguments: []
+        DOMObserver: {
+            name: 'dom.observer',
+            cls: 'Eternity.Components.DOM.Observer',
+            args: ['@element.binder', '@input.resolver']
+        },
+        DataProvider: {
+            name: 'provider.data',
+            cls: 'Eternity.Components.Provider.Data',
+            args: ['@dom.repository', '@element.crawler']
         }
     },
-    handlers: [
-        
+    inputHandlers: [
+        {
+            name: 'input.handler.calculus',
+            cls: 'Eternity.Components.Input.Handler.Calculus',
+            args: ['@provider.data', '@mapper', '@element.crawler']
+        }
     ],
-    resultHandlers: [
-        
+    outputHandlers: [
+        {
+            name: 'output.handler.value-updater',
+            cls: 'Eternity.Components.Output.Handler.ValueUpdater',
+            arts: ['@dom.repository']
+        }
+    ],
+    misc: [
+        {
+            name: 'mapper',
+            cls: 'Eternity.Helper.Mapper',
+            args: []
+        },
+        {
+            name: 'query-builder',
+            cls: 'Eternity.Helper.QueryBuilder',
+            args: []
+        },
+        {
+            name: 'dom.crawler',
+            cls: 'Eternity.Components.DOM.Crawler',
+            args: ['@query-builder']
+        }
     ]
 };
 
-var EternityApp = {
-    _core: {
+Eternity.App = function(){
+    /**
+     * @type Eternity.Container.Container
+     */
+    var _container = new Eternity.Container.Container();
+    
+    /**
+     * @type JSON
+     */
+    var _core = {};
+    
+    /**
+     * Constructor
+     */
+    var _construct = function(){
+        //register all dependencies,
+        //core, handlers, misc
+        _registerDependencies();
+        //create system components
+        //core, handlers, but no misc
+        _createComponents();
+    };
+    
+    /**
+     * Proxy method for DOMObserver.observe
+     * 
+     * @param {Elementp[]} elements - elements to observe
+     * @param {String[]} events - events to observe
+     * @returns {Eternity.App}
+     */
+    this.observe = function(elements, events){
+        _core.DOMRepository.add(elements);
+        _core.DOMObserver.observe(elements, events);
         
-    },
-    _domRepository: null,
-    _elementCrawler: null,
-    _resolver: null,
-    _resultResolver: null,
-    _domObserver: null,
-    _dataProvider: null,
-    initialize: function(){
+        return this;
+    };
+    
+    /**
+     * Get service by name
+     * 
+     * @param {String} name - service name
+     * @returns {mixed}
+     */
+    this.getService = function(name){
+        return _container.create(name);
+    };
+    
+    /**
+     * Register dependencies in container
+     * @returns {Eternity.App}
+     */
+    var _registerDependencies = function(){
+        var config = _getConfig();
         
-    }
+        _registerCoreDependencies(config.core);
+        _register(config.inputHandlers);
+        _register(config.outputHandlers);
+        _register(config.misc);
+        
+        return this;
+    };
+    
+    /**
+     * Register core dependencies
+     * 
+     * @param {JSON} config - config @see Eternity.config.core
+     */
+    var _registerCoreDependencies = function(config){
+        var key;
+    
+        for(key in config){
+            _container.register(config[key].name, config[key].cls, config[key].args);
+        }
+    };
+    
+    /**
+     * Register array of services
+     * 
+     * @param {JSON} items - services to register
+     */
+    var _register = function(items){
+        var i;
+        
+        for(i = 0; i < items.length; i++){
+            _container.register(items[i].name, items[i].cls, items[i].args);
+        }
+    };
+    
+    /**
+     * Create all required components
+     */
+    var _createComponents = function(){
+        var config = _getConfig();
+        
+        _createCoreComponents(config.core);
+        _createInputHandlers(config.inputHandlers);
+        _createOutputHandlers(config.outputHandlers);
+    };
+    
+    /**
+     * Create core application components
+     * 
+     * @param {JSON} config
+     */
+    var _createCoreComponents = function(config){
+        var key;
+        
+        for(key in config){
+            _core[key] = _container.create(config[key].name);
+        }
+    };
+    
+    /**
+     * Create input handlers
+     * 
+     * @param {JSON[]} items - input handlers config
+     */
+    var _createInputHandlers = function(items){
+        var i;
+        
+        for(i = 0; i < items.length; i++){
+            _core.InputResolver.registerHandler(_container.create(items[i].name));
+        }
+    };
+    
+    /**
+     * Create output handlers
+     * 
+     * @param {JSON[]} items - handlers config
+     */
+    var _createOutputHandlers = function(items){
+        var i;
+        
+        for(i = 0; i < items.length; i++){
+            _core.OutputResolver.registerHandler(_container.create(items[i].name));
+        }
+    };
+    
+    /**
+     * Get config
+     * 
+     * @returns {Eternity.config}
+     */
+    var _getConfig = function(){
+        return Eternity.config;
+    };
+    
+    _construct.call(this);
 };
