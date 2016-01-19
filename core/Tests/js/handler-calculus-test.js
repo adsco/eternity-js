8b0d35a4d5141741c8c0631e7f527eebb0b61522
Yu <eternitywisher@gmail.com>
2016-01-19 19:26:50 +1100

    Fixed calculus handler test case

--- core/Tests/js/handler-calculus-test.js
+++ core/Tests/js/handler-calculus-test.js
@@ -10,6 +10,7 @@ function getHandler(){
         {
             get: function(identifier){
                 return {
+                    target: '125',
                     field: '125',
                     handler: function(handler){
                         return 125;
@@ -50,6 +51,7 @@ function getComplexHandler(){
                     case '1': {
                         return {
                             field: '1',
+                            target: '1',
                             handler: function(handler){
                                 return handler.getValue('2') * handler.getValue('3');
                             }
@@ -58,6 +60,7 @@ function getComplexHandler(){
                     case '2': {
                         return {
                             field: '2',
+                            target: '2',
                             handler: function(handler){
                                 return handler.getValue('simple-1') + handler.getValue('simple-2');
                             }
@@ -66,6 +69,7 @@ function getComplexHandler(){
                     case '3': {
                         return {
                             field: '3',
+                            target: '3',
                             handler: function(handler){
                                 return handler.getValue('simple-3') + handler.getValue('simple-4');
                             }
@@ -87,20 +91,23 @@ QUnit.test('Testing calculus', function(assert){
     var handler = getHandler();
     
     assert.strictEqual(handler.supports(), true, 'Test supports method');
-    assert.deepEqual(handler.handle(), [{field: '125', value: 125}], 'Testing simple mapping');
+    assert.deepEqual(handler.handle(), {data: [{field: '125', value: 125}], type: 'update-value'}, 'Testing simple mapping');
 });
 
 QUnit.test('Testing complex calculus', function(assert){
     var handler = getComplexHandler();
     
-    assert.deepEqual(handler.handle(3), [{field: '3', value: 7}], '.handle(3) equals 7');
+    assert.deepEqual(handler.handle(3), {data: [{field: '3', value: 7}], type: 'update-value'}, '.handle(3) equals 7');
     assert.deepEqual(
         handler.handle(1),
-        [
-            {field: '2', value: 3},
-            {field: '3', value: 7},
-            {field: '1', value: 21}
-        ],
+        {
+            data: [
+                {field: '2', value: 3},
+                {field: '3', value: 7},
+                {field: '1', value: 21}
+            ],
+            type: 'update-value'
+        },
         '.handle(1) equals 21'
     );
 });
