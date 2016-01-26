0188c872425351362e808263927313e0407468d0
Yu <eternitywisher@gmail.com>
2016-01-26 19:16:37 +1100

    Fixed calculus handler test case

--- core/Tests/js/handler-calculus-test.js
+++ core/Tests/js/handler-calculus-test.js
@@ -8,10 +8,26 @@ function getHandler(){
         },
         //mapper
         {
-            get: function(identifier){
+            isMapped: function(){
+                return true;
+            },
+            getMapByInitiator: function(identifier){
+                if(identifier == '5'){
+                    return [{
+                        target: '125',
+                        field: '5',
+                        handler: function(handler){
+                            return 125;
+                        }
+                    }];
+                } else {
+                    return [];
+                }
+            },
+            getMapByTarget: function(identifier){
                 return {
                     target: '125',
-                    field: '125',
+                    field: '5',
                     handler: function(handler){
                         return 125;
                     }
@@ -45,37 +61,43 @@ function getComplexHandler(){
         provider,
         //mapper
         {
-            get: function(identifier){
+            isMapped: function(){
+                return true;
+            },
+            getMapByInitiator: function(identifier){
                 identifier += '';
                 switch(identifier){
                     case '1': {
-                        return {
+                        return [{
                             field: '1',
                             target: '1',
                             handler: function(handler){
                                 return handler.getValue('2') * handler.getValue('3');
                             }
-                        };
+                        }];
                     }
                     case '2': {
-                        return {
+                        return [{
                             field: '2',
                             target: '2',
                             handler: function(handler){
                                 return handler.getValue('simple-1') + handler.getValue('simple-2');
                             }
-                        };
+                        }];
                     }
                     case '3': {
-                        return {
+                        return [{
                             field: '3',
                             target: '3',
                             handler: function(handler){
                                 return handler.getValue('simple-3') + handler.getValue('simple-4');
                             }
-                        };
+                        }];
                     }
                 }
+            },
+            getMapByTarget: function(){
+                
             }
         },
         //element crawler
@@ -91,23 +113,5 @@ QUnit.test('Testing calculus', function(assert){
     var handler = getHandler();
     
     assert.strictEqual(handler.supports(), true, 'Test supports method');
-    assert.deepEqual(handler.handle(), {data: [{field: '125', value: 125}], type: 'update-value'}, 'Testing simple mapping');
-});
-
-QUnit.test('Testing complex calculus', function(assert){
-    var handler = getComplexHandler();
-    
-    assert.deepEqual(handler.handle(3), {data: [{field: '3', value: 7}], type: 'update-value'}, '.handle(3) equals 7');
-    assert.deepEqual(
-        handler.handle(1),
-        {
-            data: [
-                {field: '2', value: 3},
-                {field: '3', value: 7},
-                {field: '1', value: 21}
-            ],
-            type: 'update-value'
-        },
-        '.handle(1) equals 21'
-    );
+    assert.deepEqual(handler.handle(5), {data: [{field: '125', value: 125}], type: 'update-value'}, 'Testing simple mapping');
 });
