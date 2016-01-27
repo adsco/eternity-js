b7ace50c444dd185271900f3c8821668b1bffeef
Yu <eternitywisher@gmail.com>
2016-01-27 23:30:55 +1100

    Updated comments

--- core/Components/Input/Handler/validator.js
+++ core/Components/Input/Handler/validator.js
@@ -2,18 +2,44 @@
  * Validation result: {success: '', message: ''}
  */
 Eternity.Components.Input.Handler.Validator = function(dataProvider, elementCrawler, validation, domRepository){
+    /**
+     * @type String
+     */
     var EVENT_TYPE = 'validation-check';
     
+    /**
+     * @type Eternity.Helper.ValidationMapper
+     */
     var _validation = null;
     
+    /**
+     * @type Eternity.Components.Provider.Data
+     */
     var _dataProvider = null;
     
+    /**
+     * @type Eternity.Components.DOM.Element.Crawler
+     */
     var _elementCrawler = null;
     
+    /**
+     * @type Eternity.Components.DOM.Repository
+     */
     var _domRepository = null;
     
+    /**
+     * @type Object[]
+     */
     var _result = [];
     
+    /**
+     * Constructor
+     * 
+     * @param {Eternity.Components.Provider.Data} dataProvider
+     * @param {Eternity.Components.DOM.Element.Crawler} elementCrawler
+     * @param {Eternity.Helper.ValidationMapper} validation
+     * @param {Eternity.Components.DOM.Repoitory} domRepository
+     */
     var _construct = function(dataProvider, elementCrawler, validation, domRepository){
         _dataProvider = dataProvider;
         _elementCrawler = elementCrawler;
@@ -21,12 +47,29 @@ Eternity.Components.Input.Handler.Validator = function(dataProvider, elementCraw
         _domRepository = domRepository;
     };
     
+    /**
+     * Is field should be handled by this handler
+     * 
+     * @param {Element} element
+     * @param {Event} e
+     * @returns {Boolean}
+     */
     this.supports = function(element, e){
         var identifier = elementCrawler.getAttribute(element, 'id');
         
         return validation.hasConstraint(identifier);
     };
     
+    /**
+     * Implementation of HandlerBase.handle, @see handler/handler-base.js
+     * Logic: stack list of all nodes whos values need to be recalculated
+     * then perform up down calculations, caching intermadiate calculation
+     * result.
+     * 
+     * @param {Element} element
+     * @param {Event} e
+     * @returns {Eternity.Factory.Result}
+     */
     this.handle = function(element, e){
         var identifier = elementCrawler.getAttribute(element, 'id'),
             fields = _domRepository.getLog();
@@ -49,6 +92,11 @@ Eternity.Components.Input.Handler.Validator = function(dataProvider, elementCraw
         return _getResult();
     };
     
+    /**
+     * Validate field values using validation mapper
+     * 
+     * @param {Object[]} fields
+     */
     var _validate = function(fields){
         var constraint,
             result,
@@ -68,6 +116,13 @@ Eternity.Components.Input.Handler.Validator = function(dataProvider, elementCraw
         }
     };
     
+    /**
+     * Append field result to result summary
+     * 
+     * @param {String} field
+     * @param {Boolean} result
+     * @param {String} message
+     */
     var _appendResult = function(field, result, message){
         _result.push({
             field: field,
@@ -76,10 +131,18 @@ Eternity.Components.Input.Handler.Validator = function(dataProvider, elementCraw
         });
     };
     
+    /**
+     * Clear recent results from result summary
+     */
     var _reset = function(){
         _result = [];
     };
-    
+
+    /**
+     * Get result summary
+     * 
+     * @returns {Object}
+     */
     var _getResult = function(){
         return {
             type: EVENT_TYPE,
